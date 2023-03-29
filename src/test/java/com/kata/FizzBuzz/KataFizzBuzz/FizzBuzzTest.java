9014a1f1f571bb227bb4fb6bd828a46f576ad4fc
Aneebch <aneeb.chatha786@gmail.com>
2023-03-29 10:15:01 +0200

    Creating Pakages and Classes for Kata

---
+++ src/test/java/com/kata/FizzBuzz/KataFizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+package com.kata.FizzBuzz.KataFizzBuzz;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void test() {
+    }
+
+}
