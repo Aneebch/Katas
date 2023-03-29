cff440a18a87b8a248fa8ddbf6590c4af43bf693
Aneebch <aneeb.chatha786@gmail.com>
2023-03-29 12:38:53 +0200

    Test that multiply by 3

--- src/main/java/com/kata/FizzBuzz/kataFizzBuzz/FizzBuzz.java
+++ src/main/java/com/kata/FizzBuzz/kataFizzBuzz/FizzBuzz.java
@@ -1,5 +1,12 @@
 package com.kata.FizzBuzz.kataFizzBuzz;
 
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
 public class FizzBuzz {
 
+    public String execute(int i) {
+        if (i % 3 == 0)  return "Fizz";
+        return null;
+    }
 }
--- src/test/java/com/kata/FizzBuzz/KataFizzBuzz/FizzBuzzTest.java
+++ src/test/java/com/kata/FizzBuzz/KataFizzBuzz/FizzBuzzTest.java
@@ -1,7 +1,11 @@
 package com.kata.FizzBuzz.KataFizzBuzz;
 
+import com.kata.FizzBuzz.kataFizzBuzz.FizzBuzz;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class FizzBuzzTest {
 
@@ -11,7 +15,24 @@ public class FizzBuzzTest {
     }
 
     @Test
-    void test() {
+    void cuandoSeaMultiploPor3DevuelveFizz() {
+       // GIVEN FOR THE TEST
+       var fizzBuzz = new FizzBuzz();
+       // WHEN
+        var sut = fizzBuzz.execute(3);
+        // THEM
+        assertEquals("Fizz", sut);
     }
+
+    @Test
+    void cuandoSeaMultiploPor5DevuelveBuzz() {
+        // GIVEN FOR THE TEST
+        var fizzBuzz1 = new FizzBuzz();
+        // WHEN
+        var sut = fizzBuzz1.execute(5);
+        // THEM
+        assertEquals("Buzz", sut);
+    }
+
 
 }
