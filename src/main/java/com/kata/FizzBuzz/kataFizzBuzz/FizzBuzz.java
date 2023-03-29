8236b2e033cca7447cc69f1adde341ec9fa3f454
Aneebch <aneeb.chatha786@gmail.com>
2023-03-29 12:48:27 +0200

    Test for multipy of 5

--- src/main/java/com/kata/FizzBuzz/kataFizzBuzz/FizzBuzz.java
+++ src/main/java/com/kata/FizzBuzz/kataFizzBuzz/FizzBuzz.java
@@ -6,7 +6,11 @@ import lombok.EqualsAndHashCode;
 public class FizzBuzz {
 
     public String execute(int i) {
-        if (i % 3 == 0)  return "Fizz";
+        if (i % 3 == 0) {
+            return "Fizz";
+    }   if (i % 5 == 0) {
+            return "Buzz";
+    }
         return null;
     }
 }
